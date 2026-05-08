f4cb424863d32ed180389a63f38c14a627260b79
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2026-05-08 14:36:13 -0300

    update nvim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,13 +1,8 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-j>", "<C-d>zz")
-vim.keymap.set("n", "<C-k>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("n", "<C-p>", LazyVim.pick("files"))
 vim.keymap.set("n", "<C-f>", LazyVim.pick("live_grep"))
-
-vim.keymap.set("n", "<C-Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
-vim.keymap.set("n", "<C-S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
